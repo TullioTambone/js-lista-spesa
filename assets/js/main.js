322edae76005729b15e24ebf961c6357084fd81b
TullioTambone <tulliotambone97@gmail.com>
2023-03-30 09:55:43 +0200

    fine

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,8 +8,13 @@ let spesa = [
 
 
 let i = 0;
+const mioUl = document.getElementById('sonoIo');
 
-while( i < spesa.length){
-    console.log(spesa[i]);
+while( i < spesa.length ){
+
+    let listaItem = document.createElement('li');
+    listaItem.innerHTML = `${spesa[i]}`
+    mioUl.append(listaItem);
+
     i++;
 }
